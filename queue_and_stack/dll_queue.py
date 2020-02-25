912308e9f332978f17cbc614fe058329a061df9d
lowell1 <29132034+lowell1@users.noreply.github.com>
2020-02-24 21:40:34 -0800

    dll_queue.py

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,14 +6,13 @@ from doubly_linked_list import DoublyLinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.list = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.list.add_to_head(value)
 
     def dequeue(self):
-        pass
+        return self.list.remove_from_tail()
 
     def len(self):
-        pass
+        return len(self.list)
